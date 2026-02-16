b5bba9110b3dffccbba19fd3360588f6f2abbb7a
Fermy <123935594+KarnakOza@users.noreply.github.com>
2026-02-16 20:00:05 +0530

    Update workflow.py

--- docs/workflow.py
+++ docs/workflow.py
@@ -7,3 +7,14 @@ parameters.put('polyDegree', '3')
 parameters.put('continueOnFail', 'false')
 apply_orbit_file = GPF.createProduct('Apply-Orbit-File', parameters, product)
 return imgplot
+
+
+-#/for GRDH file terrain correction 
+
+parameters = HashMap()
+parameters.put('demName', 'SRTM 3Sec')
+parameters.put('pixelSpacingInMeter', 10.0)
+parameters.put('sourceBands', 'Sigma0_VV')
+speckle_filter_tc = GPF.createProduct("Terrain-Correction", parameters,
+speckle_filter)
+plotBand(speckle_filter_tc, 'Sigma0_VV', 0, 0.1)
